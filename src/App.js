b162cb218d017a4ea127b12a70a654c3493b8f73
Zeus Amorim <amorimzeus@gmail.com>
2022-09-30 01:26:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [NOTICIAS, setNoticias] = useState([]);
+
+
+
+  useEffect(() => {
+    async function () {
+      axios('http://localhost:8000/noticias_anderson')
+        .then(response => {
+          setNoticias(response.data)
+          console.log(response.data)
+
+        })
+        .catch(error => console.log(error))
+    }
+  }, [NOTICIAS])
+
   return (
     <div className="App">
-      
+
+      <h1>Noticias</h1>
+
     </div>
   );
 }
