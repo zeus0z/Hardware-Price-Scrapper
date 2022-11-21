2b7dea86bd2ed17dd02191dcc674f4a102b12677
Zeus Amorim <amorimzeus@gmail.com>
2022-11-21 00:25:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-const fs = require('fs')
-const puppeteer = require('puppeteer')
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 
-const URL = 'https://www.pichau.com.br/hardware/placa-de-video'
+const URL = 'https://www.pichau.com.br/hardware/placa-de-video';
 
-function extractItems() {
-    
-      const extractedElements = document.querySelectorAll();
-      const items = [];
-      for (let element of extractedElements) {
-        items.push(element);
-      }
-      return items;
+
+
+(async () => {
+
+    try {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage();
+
+        await page.goto(URL, {
+            waitUntil: "domcontentloaded",
+            timeout: 60000
+        });
+
+        const mySelector = '.MuiTypography-h6'
+
+        await page.waitForSelector('.MuiTypography-h6');
+
+        const result = await page.evaluate(() => {
+
+
+            const items = Array.from(
+                document.querySelectorAll('.MuiTypography-h6')
+            )
+
+            let filtro = [];
+
+            for (let i of items) {
+                filtro.push(
+                    // i.getAttribute('src')
+                    i.innerText
+                )
+            }
+
+            return filtro;
+
+
+        })
+
+        console.log(result);
+        await browser.close();
+
+
+    } catch (e) {
+        console.log(e.message)
     }
+}
 
 
-(async ()=>{
- const browser = await puppeteer.launch()
- const page = await browser.newPage();
- await page.goto(URL);
 
- await page.evaluate(()=>{
-    
- })
 
-})();
+
+)();
